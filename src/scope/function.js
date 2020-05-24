fe57b139181a4a1cc8251504f5a77959b97e6acc
Razielini <razielco@gmail.com>
2020-05-24 12:43:17 -0600

    Ejemplo Block

---
+++ src/scope/function.js
@@ -0,0 +1,18 @@
+const fruits = () => {
+  var fruit = 'apple'
+  console.log('fruits :: fruit ::', fruit)
+}
+
+fruits()
+console.log('fruit ::', fruit)
+
+const anotherFunction = () => {
+  var x = 1
+  var x = '2'
+  let y = 1
+  // let y = 2
+  console.log({x})
+  console.log(y)
+}
+
+anotherFunction()
