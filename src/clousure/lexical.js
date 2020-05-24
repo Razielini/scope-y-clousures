ff30225883e74f665b023f4e98b1de271e44756d
Razielini <razielco@gmail.com>
2020-05-24 13:32:23 -0600

    Create lexical.js

---
+++ src/clousure/lexical.js
@@ -0,0 +1,18 @@
+const buildCount = (i) => {
+  let count = i
+
+  const displayCount = () => {
+    console.log(count++)
+  }
+
+  return displayCount
+}
+
+
+const myCount = buildCount(1)
+myCount()
+myCount()
+
+const myOtherCount = buildCount(10)
+myOtherCount()
+myOtherCount()
